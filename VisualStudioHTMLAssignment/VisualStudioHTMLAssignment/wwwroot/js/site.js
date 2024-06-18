ca257c51191d133a79f8534f0a1b4f65a6322373
Kyle Rankin <krankin@gmail.com>
2024-06-18 17:52:11 -0500

    VisualStudioHTMLAssignment added
    VisualStudioHTMLAssignment added with basic HTML page including YouTube video, modified CSS, and JS animation

---
+++ VisualStudioHTMLAssignment/VisualStudioHTMLAssignment/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+//Header animation
+function newColor() {
+    document.getElementById("header").style.color = "darkblue";
+}
+function oldColor() {
+    document.getElementById("header").style.color = "black";
+}
